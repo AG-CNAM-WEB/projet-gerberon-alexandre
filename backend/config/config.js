8d08941453654fe534ac135c5bd38c037a23408d
max1lock <alex.gerberon@gmail.com>
2024-06-19 10:23:54 +0200

    add prod config

--- backend/config/config.js
+++ backend/config/config.js
@@ -8,4 +8,16 @@ module.exports = {
     host: process.env.DB_HOST,
     dialect: 'postgres',
   },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    protocol: "postgres",
+    dialectOptions: {
+      ssl: true,
+      native: true,
+    },
+  }
 };
